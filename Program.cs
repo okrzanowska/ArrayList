e769f784e7cef8f6d5baca96ea6c5d8c1ee30e79
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 13:28:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayList
+{    
+    class Program
+    {
+        private static void Main()
+        {
+            var cities = new ArrayList() { "Gdańsk", "Warszawa", "Katowice", "Poznań", "Kraków", "Rzeszów" };
+            cities.Sort();
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
